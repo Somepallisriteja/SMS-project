7bf953f3d65bd8c845180e01aee3f7a716241f65
ssomepalli <somepalli.sriteja@legalhorizon.ag>
2019-02-05 15:16:33 +0100

    Tasks

--- taskapp/src/app/form/form.component.ts
+++ taskapp/src/app/form/form.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component} from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.css']
 })
-export class FormComponent implements OnInit {
+export class FormComponent {
 
-  constructor() { }
-
-  ngOnInit() {
+  email = new FormControl('', [Validators.required, Validators.email]);
+  password = new FormControl('', [Validators.required]);
+  hide = true;
+  getErrorMessage() {
+    return this.email.hasError('required') ? 'You must enter a value' :
+        this.email.hasError('email') ? 'Not a valid email' :
+            '';
   }
-
+  getErrorMessage1() {
+    
+        
+            '';
+  }
 }
